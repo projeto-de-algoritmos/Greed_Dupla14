645bc0814e770e4ff92a97f1131dcf6930171932
FranciscoHeronildo <heronildo.2010@hotmail.com>
2022-03-05 23:03:36 -0300

    Finalizado algoritimo knapsack
    Co-authored-by: Natilorens <nathalialorenacardoso@gmail.com>

--- src/1286.py
+++ src/1286.py
@@ -11,4 +11,21 @@ def knapSack(W, wt, val, n):
             val[n-1] + knapSack(W-wt[n-1], wt, val, n-1),
             knapSack(W, wt, val, n-1)
         )
-        
+
+
+while True:
+    val = []
+    wt = []
+    numberofOrders = int(input())
+
+    if (not numberofOrders):
+        break
+
+    maxOrders = int(input())
+
+    for i in range(numberofOrders):
+        line = list(map(int, input().split()))
+        val.append(line[0])
+        wt.append(line[1])
+
+    print("{} min.".format(knapSack(maxOrders, wt, val, len(val))))
