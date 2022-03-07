e0f69260e597ea1a3066ab240b209e1accb53166
FranciscoHeronildo <heronildo.2010@hotmail.com>
2022-03-07 01:18:56 -0300

    Finalizando resolução
    Co-authored-by: Natilorens <nathalialorenacardoso@gmail.com>

--- src/1288.cpp
+++ src/1288.cpp
@@ -13,11 +13,37 @@ int main()
     for (i = 1; i <= casos; i++)
     {
         scanf("%d", &n);
-        for (j = 1; j <= n; j++){
+        for (j = 1; j <= n; j++)
+        {
             scanf("%d %d", &dano[j], &peso[j]);
         }
         scanf("%d %d", &k, &r);
-        for (j = 0; j < k; j++){
+        for (j = 0; j < k; j++)
+        {
             w[0][j] = 0;
         }
-        
+        for (j = 1; j <= n; j++)
+        {
+            w[j][0] = 0;
+            for (c = 1; c <= k; c++)
+                if (peso[j] > c)
+                {
+                    w[j][c] = w[j - 1][c];
+                }
+                else
+                {
+                    w[j][c] = max(w[j - 1][c - peso[j]] + dano[j], w[j - 1][c]);
+                }
+        }
+        if (w[n][k] >= r)
+        {
+            printf("Missao completada com sucesso\n");
+        }
+        else
+        {
+            printf("Falha na missao\n");
+        }
+    }
+
+    return 0;
+}
