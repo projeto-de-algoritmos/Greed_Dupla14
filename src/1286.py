22526500a8ef851d0c39b99560a2fcccdeb85e36
FranciscoHeronildo <heronildo.2010@hotmail.com>
2022-03-07 19:38:13 -0300

    Adicionando comentarios no 1286

--- src/1286.py
+++ src/1286.py
@@ -1,11 +1,21 @@
+# Retorna o valor maximo que pode ser colocado
+# em uma Mochila de capacidade W
+
+# Funca que implementa o algoritmo da Mochila
 def knapSack(W, wt, val, n):
 
+    # Caso base
     if n == 0 or W == 0:
         return 0
 
+    # Se o peso do enesimo item for mais que Mochila de capacidade W,
+    # entao este item nao pode ser incluido na solucao otima
     if (wt[n-1] > W):
         return knapSack(W, wt, val, n-1)
 
+    # retorna o maximo de dois casos:
+    # (1) enesimo item incluido
+    # (2) nao incluido
     else:
         return max(
             val[n-1] + knapSack(W-wt[n-1], wt, val, n-1),
@@ -13,19 +23,28 @@ def knapSack(W, wt, val, n):
         )
 
 
-while True:
-    val = []
-    wt = []
-    numberofOrders = int(input())
+# Inicializa a Mochila vazia
+val = []
+wt = []
 
-    if (not numberofOrders):
+while True:
+
+    # N numero de pedidos
+    n = input()
+    n = int(n)
+
+    if (not n):
         break
 
-    maxOrders = int(input())
+    # P numero maximo de pizzas
+    p = input()
+    p = int(p)
 
-    for i in range(numberofOrders):
+    # Adiciona os itens a Mochila
+    for i in range(n):
         line = list(map(int, input().split()))
         val.append(line[0])
         wt.append(line[1])
-
-    print("{} min.".format(knapSack(maxOrders, wt, val, len(val))))
+    
+    # Imprimi a saida
+    print('\n {} min.'.format(knapSack(p, wt, val, len(val))))
