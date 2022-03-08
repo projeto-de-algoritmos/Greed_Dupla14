0f9b2dbc136f5f454d8dc7451c312105b40b800f
Nathalia Lorena <nathalialorenacardoso@gmail.com>
2022-03-07 21:51:15 -0300

    Adicionando comentários

--- src/1288.cpp
+++ src/1288.cpp
@@ -9,15 +9,21 @@ int main()
     int w[100][100];
     int casos, i, j, n, k, r, c;
 
+    // Quantidade de casos de teste
     scanf("%d", &casos);
     for (i = 1; i <= casos; i++)
-    {
+    {   
+        // Leitura da quantidade de projéteis 
         scanf("%d", &n);
         for (j = 1; j <= n; j++)
         {
+            // Leitura do dano do projétil e do peso dele
             scanf("%d %d", &dano[j], &peso[j]);
         }
+        // Leitura da capacidade de carga do canhão e resistência total do castelo
         scanf("%d %d", &k, &r);
+
+        // Iniciando algoritimo da mochila
         for (j = 0; j < k; j++)
         {
             w[0][j] = 0;
@@ -35,10 +41,12 @@ int main()
                     w[j][c] = max(w[j - 1][c - peso[j]] + dano[j], w[j - 1][c]);
                 }
         }
+        // Se o dano total das cargas carregadas for maior ou igual à resistência do castelo
         if (w[n][k] >= r)
         {
             printf("Missao completada com sucesso\n");
         }
+        // Caso contrário
         else
         {
             printf("Falha na missao\n");
